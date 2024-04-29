d01e87cad4a02605fb08240804df92d77bf5e40c
Dan Lee <danielhlee4@gmail.com>
2024-04-29 16:46:02 -0400

    add user migration and model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email,
+        presence: true,
+        uniqueness: true, 
+        length: { in: 3..255 }, 
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password,
+        length: { in: 6..255 }, allow_nil: true
+end
