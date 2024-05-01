e2446eba27b9f01742ae9d1396d9eacb5d73bdde
Dan Lee <danielhlee4@gmail.com>
2024-05-01 14:42:02 -0400

    add ticket dashboard view, refactor flash messages to module

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to root_path, notice: 'Logged in successfully'
+            redirect_to tickets_path, notice: 'Logged in successfully'
         else
             flash.now[:alert] = 'Email or password is invalid'
             render :new
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        session.delete(:user_id)
+        session[:user_id] = nil
         redirect_to login_path, notice: 'Logged out successfully'
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def render_flash
+        flash.map do |key, value|
+            content_tag :div, class: "alert alert-#{key == 'notice' ? 'success' : 'danger'} alert-dismissible fade show", role: 'alert' do
+                concat value
+                concat(
+                content_tag(:button, '', type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' }, aria: { label: 'Close' })
+                )
+            end
+        end.join.html_safe
+    end
 end
