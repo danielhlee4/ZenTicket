b8c5e93bc12215c519843a8b3e1baf31efa99305
Dan Lee <danielhlee4@gmail.com>
2024-05-03 14:30:22 -0400

    refactor datatable to display only priority level text

--- app/assets/javascripts/tickets_index.js
+++ app/assets/javascripts/tickets_index.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     $('#tickets-table').DataTable({
         "order": [[0, 'asc'], [3, 'desc']], // Sort by user id and then by status
         "columnDefs": [
-            { "orderable": false, "targets": [4] } // Disable sorting for the Actions column
+            { "orderable": false, "targets": [4] }, // Disable sorting for the Actions column
+            {
+                "targets": 2, // The index of the priority column
+                "render": function(data, type, row, meta) {
+                    return data.substring(4); // Hide the first four characters
+                }
+            }
         ]
     });
 });
